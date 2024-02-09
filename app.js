0f4696b008902ba2a775483f1c3d5a00650de23a
aanchal <aanchalverma721@gmail.com>
2024-02-09 17:01:23 +0530

    whitelist added to prevent the cors error coming from front end

--- app.js
+++ app.js
@@ -3,10 +3,25 @@ const getConnect = require('./src/utils/dbConnect')
 const userRoute=require('./src/Routes/userRoutes')
 const bodyParser=require('body-parser')
 const app=express()
+const cors = require('cors')
 getConnect()
 app.use(bodyParser.json())
 // app.use(bodyParser.urlencoded({extended:true}))
+const whiteList = ['http://localhost:3000/', 'http://localhost:3000','http://localhost:3001']
+const inableCors = {
+  origin: (origin, callback) => {
+    if (whiteList.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Request from unauthorized origin"));
+    }
+  },
+};
+
+
+app.use(cors(inableCors))
 app.use('/user',userRoute)
+app.use('cors')
 app.all('*', async (req, res) => {
     try {
         console.log(req.url);
